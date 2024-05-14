cb16ba3c557745303cf8eb1d11f85a1ce6f3e57f
David Vaillant <david@vaillant.io>
2024-05-13 23:12:41 -0400

    README. Sertves at port 5050.

--- download_models.py
+++ download_models.py
@@ -1,17 +1,18 @@
 from pathlib import Path
 from huggingface_hub import hf_hub_download
 
-
+# If you want to use IQ3, uncomment it.
 MODELS = [
-    ('bartowski/Meta-Llama-3-8B-Instruct-GGUF', 'Meta-Llama-3-8B-Instruct-IQ3_XXS.gguf'),
+    # ('bartowski/Meta-Llama-3-8B-Instruct-GGUF', 'Meta-Llama-3-8B-Instruct-IQ3_XXS.gguf'),
     ('bartowski/Meta-Llama-3-8B-Instruct-GGUF', 'Meta-Llama-3-8B-Instruct-Q3_K_M.gguf'),
     ('vikhyatk/moondream2', 'moondream2-mmproj-f16.gguf'),
     ('vikhyatk/moondream2', 'moondream2-text-model-f16.gguf')
 ]
 
-for REPO_ID, FILENAME in MODELS:
-    if not (Path('models')/FILENAME).exists():
-        print(f"Downloading {FILENAME}...")
-        hf_hub_download(repo_id=REPO_ID, filename=FILENAME, local_dir='models')
-    else:
-        print(f"{FILENAME} already downloaded.")
+if __name__ == "__main__":
+    for REPO_ID, FILENAME in MODELS:
+        if not (Path('models')/FILENAME).exists():
+            print(f"Downloading {FILENAME}...")
+            hf_hub_download(repo_id=REPO_ID, filename=FILENAME, local_dir='models')
+        else:
+            print(f"{FILENAME} already downloaded.")
--- wsgi.py
+++ wsgi.py
@@ -3,4 +3,4 @@ import petrock
 app = petrock.webapp.app
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=5050)
